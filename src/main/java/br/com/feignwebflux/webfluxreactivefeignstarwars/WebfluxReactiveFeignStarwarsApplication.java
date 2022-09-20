c1490eecc9e51b8913d2e68a1a3822d61fbe3604
Bruna Iriz <45856882+bruna-iriz@users.noreply.github.com>
2022-09-20 13:34:07 -0300

    criando estrutura inicial

---
+++ src/main/java/br/com/feignwebflux/webfluxreactivefeignstarwars/WebfluxReactiveFeignStarwarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.feignwebflux.webfluxreactivefeignstarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxReactiveFeignStarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxReactiveFeignStarwarsApplication.class, args);
+	}
+
+}
